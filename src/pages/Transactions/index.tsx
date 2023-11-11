e2693a0aec36a6c944ad9aeae7a423ea5cc1901f
Lubnnia <lubnnia@gmail.com>
2023-11-11 17:10:09 -0300

    First commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+
+function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+    </div>
+  );
+}
+
+export { Transactions };
